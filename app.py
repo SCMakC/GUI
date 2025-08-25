ded39348a67435ec96eea0e7e45587f86cd6a587
SCMakC <SCORPXMakC@yandex.ru>
2025-08-25 17:33:16 +0600

    Первый коммит_1

---
+++ app.py
@@ -0,0 +1,7 @@
+import my_module
+
+print("Привет")
+
+x = my_module.summa(4,5)
+
+print(x)
---
+++ my_module.py
@@ -0,0 +1,7 @@
+print("Hello? module")
+
+pi= 3.14
+
+def summa(a,b):
+    """Складывает два числа"""
+    return a+b
